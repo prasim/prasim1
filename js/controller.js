973c23803a12a3b496154a0f95df8fc084d79480
Prasim <prasimjain1@Prasims-MacBook-Pro.local>
2016-04-29 11:23:07 +0530

    adding changes to autocomplete div

--- js/controller.js
+++ js/controller.js
@@ -3,12 +3,16 @@ app.controller('myCtrl', function($scope,$http) {
     $scope.searchText = "";
  
     $scope.search = function(searchText) {
-        $scope.closeSuggestions = false;
-        var url = "http://www.omdbapi.com/?";
-        url = url + 's=' + searchText;
-        $http.get(url).success( function(response) {
-            $scope.results = response.Search;
-        });
+        if(searchText.length){
+            $scope.closeSuggestions = false;
+            var url = "http://www.omdbapi.com/?";
+            url = url + 's=' + searchText;
+            $http.get(url).success( function(response) {
+                $scope.results = response.Search;
+            });
+        } else {
+            $scope.closeSuggestions = true;
+        }
     }
 
     $scope.selectMovie = function(name) {
